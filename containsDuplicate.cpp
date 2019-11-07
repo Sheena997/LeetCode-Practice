a744e1a3247aebe1128e36c63a57d1eb19f02888
Sheena爽 <18365368008@163.com>
2019-11-07 18:28:01 +0800

    施爽：于2019年11月7日上传的判断一个数组中是否存在重复元素（排序）的代码。

---
+++ containsDuplicate.cpp
@@ -0,0 +1,24 @@
+/*
+存在重复元素
+给定一个整数数组，判断是否存在重复元素。
+如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+示例 1:
+输入: [1,2,3,1]
+输出: true
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/contains-duplicate
+*/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if(nums.empty())
+            return false;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 1; ++i)
+        {
+            if(nums[i] == nums[i + 1])
+                return true;
+        }
+        return false;
+    }
+};
