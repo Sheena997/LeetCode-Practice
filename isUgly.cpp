f3b9f18bf2931f509e214b5397bbdbe855b0c207
Sheena爽 <18365368008@163.com>
2019-11-18 21:41:22 +0800

    施爽：于2019年11月18日上传的判断一个数是否只有2，3，5质因子的代码。

---
+++ isUgly.cpp
@@ -0,0 +1,33 @@
+/*
+
+编写一个程序判断给定的数是否为丑数。
+丑数就是只包含质因数 2, 3, 5 的正整数。
+示例 1:
+输入: 6
+输出: true
+解释: 6 = 2 × 3
+示例 2:
+输入: 8
+输出: true
+解释: 8 = 2 × 2 × 2
+示例 3:
+输入: 14
+输出: false 
+解释: 14 不是丑数，因为它包含了另外一个质因数 7。
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/ugly-number
+*/
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num < 1)
+            return false;
+        while(num % 5 == 0)
+            num /= 5;
+        while(num % 3 == 0)
+            num /= 3;
+        while(num % 2 == 0)
+            num /= 2;
+        return num == 1;
+    }
+};
