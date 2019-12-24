6bcbb728887e082b2e9fd6311ca68eda3227d19d
Sheena爽 <18365368008@163.com>
2019-12-24 15:24:53 +0800

    施爽：于2019年12月24日上传的set去重  vector中的assign和find的函数的代码。

---
+++ intersection.cpp
@@ -0,0 +1,31 @@
+/*
+两个数组的交集
+给定两个数组，编写一个函数来计算它们的交集。
+示例 1:
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2]
+示例 2:
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [9,4]
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-arrays
+*/
+//set去重  vector中的assign是用括号里面的容器里的东西替代vector里的东西
+//vector中的assign替换其当前内容，并相应地修改其大小
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        set<int> s;
+        for(int i = 0; i < nums1.size(); ++i)
+        {
+            if(find(nums2.begin(), nums2.end(), nums1[i]) != nums2.end())
+                s.insert(nums1[i]);
+        }
+        /*for(auto e : s)
+            res.push_back(e);
+         */
+        res.assign(s.begin(), s.end());
+        return res;
+    }
+};
