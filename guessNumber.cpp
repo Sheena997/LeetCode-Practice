22434dadfaf370bbab6126a2c2cc000779bd9d03
Sheena爽 <18365368008@163.com>
2020-01-23 14:13:40 +0800

    施爽：于2020年1月23日上传的猜数字大小的代码。

---
+++ guessNumber.cpp
@@ -0,0 +1,41 @@
+/*
+我们正在玩一个猜数字游戏。 游戏规则如下：
+我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+每次你猜错了，我会告诉你这个数字是大了还是小了。
+你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+-1 : 我的数字比较小
+ 1 : 我的数字比较大
+ 0 : 恭喜！你猜对了！
+示例 :
+输入: n = 10, pick = 6
+输出: 6
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/guess-number-higher-or-lower
+*/
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        if(n == 1)
+            return 1;
+        
+        long int left = 1;
+        long int right = n;
+        while(left <= right)
+        {
+            long int mid = left + (right - left) / 2;
+            if(guess(mid) == 0)
+                return mid;
+            else if(guess(mid) == -1)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        
+        return 0;
+    }
+};
