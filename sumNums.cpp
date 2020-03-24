61a45320b62a91aaf39fb97d00cc29c143027ffb
Sheena爽 <18365368008@163.com>
2020-03-24 21:07:48 +0800

    施爽：于2020年3月24日上传的1++n不用if、for乘除(n&&n+=f(n-1)或构造n个对象)的代码。

--- sumNums.cpp
+++ sumNums.cpp
@@ -13,13 +13,89 @@
 来源：LeetCode
 链接：https://leetcode-cn.com/problems/qiu-12n-lcof/
 */
+//方法一：帅气！
+/*
 class Solution {
 public:
     int sumNums(int n) {
         //return ((int)pow(n, 2) + n) >> 1;
-        
         n && (n += sumNums(n - 1));
         
         return n;
     }
 };
+*/
+//方法二：构造函数
+class conSum
+{
+public:
+    conSum()
+    {
+        ++n;
+        sum += n;
+    }
+    static int GetSum()
+    {
+        return sum;
+    }
+    static int n;
+    static int sum;
+};
+int conSum::n = 0;//静态成员在类外进行初始化
+int conSum::sum = 0;
+class Solution {
+public:
+    int sumNums(int n) {
+        conSum::n = 0;
+        conSum::sum = 0;
+        conSum *res = new conSum[n];//构造n个对象
+        
+        return conSum::GetSum();
+    }
+};
+
+
+
+//方法一：帅气！
+/*
+class Solution {
+public:
+    int sumNums(int n) {
+        //return ((int)pow(n, 2) + n) >> 1;
+        n && (n += sumNums(n - 1));
+        
+        return n;
+    }
+};
+*/
+//方法二：构造函数
+class conSum
+{
+public:
+    conSum()
+    {
+        ++n;
+        sum += n;
+    }
+    static int GetSum()
+    {
+        return sum;
+    }
+    static int n;
+    static int sum;
+};
+int conSum::n = 0;//静态成员在类外进行初始化
+int conSum::sum = 0;
+class Solution {
+public:
+    int sumNums(int n) {
+        conSum::n = 0;
+        conSum::sum = 0;
+        conSum *res = new conSum[n];//构造n个对象
+        
+        return conSum::GetSum();
+    }
+};
+
+
+
