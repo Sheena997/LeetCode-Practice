4c6ca1acd5793cf77d4af6448c5606d9df84aac8
Sheena爽 <18365368008@163.com>
2020-05-07 22:25:37 +0800

    施爽：于2020年5月7日上传的递归求A*B(A个B相加或B个A相加,每次取小的数当次数)的代码。

---
+++ multiplyAB.cpp
@@ -0,0 +1,29 @@
+/*
+面试题 08.05. 递归乘法
+递归乘法。 写一个递归函数，不使用 * 运算符， 
+实现两个正整数的相乘。可以使用加号、减号、位移，
+但要吝啬一些。
+示例1:
+ 输入：A = 1, B = 10
+ 输出：10
+示例2:
+ 输入：A = 3, B = 4
+ 输出：12
+提示:
+保证乘法范围不会溢出
+
+来源：LeetCode
+链接：https://leetcode-cn.com/problems/recursive-mulitply-lcci/
+*/
+class Solution {
+public:
+    int multiply(int A, int B) {
+        if(A == 0 || B == 0)
+            return 0;
+        
+        if(A < B)
+            return B +  multiply(A - 1, B);
+        else
+            return A +  multiply(A, B - 1);
+    }
+};
